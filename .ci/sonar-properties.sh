c00cd30f618b3c1d677dc003471efc570c150329
annguyen <snowflower1408@gmail.com>
2024-07-22 01:49:10 +0700

    ci: add static properties to sonar

--- .ci/sonar-properties.sh
+++ .ci/sonar-properties.sh
@@ -3,12 +3,12 @@
 NEW_VERSION=$(npx standard-version --dry-run | grep 'tagging release' | tail -n1 | awk '{print $4}')
 SONAR_CLOUD_FILE=.sonarcloud.properties
 
-# Check if NEW_VERSION is set and update sonar-project.properties
+cp .ci/$SONAR_CLOUD_FILE.tmp $SONAR_CLOUD_FILE
+
 if [[ -n "$NEW_VERSION" ]]; then
-  echo -e "sonar.projectVersion=$NEW_VERSION" > $SONAR_CLOUD_FILE
+  echo -e "sonar.projectVersion=$NEW_VERSION" >> $SONAR_CLOUD_FILE
 else
-  echo -e "sonar.projectVersion=latest" > $SONAR_CLOUD_FILE
+  echo -e "sonar.projectVersion=latest" >> $SONAR_CLOUD_FILE
 fi
 
-# Add sonar-project.properties to git
 git add $SONAR_CLOUD_FILE
